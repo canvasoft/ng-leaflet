66d0560402e17ca4f9e65639b442ef2dd06254a2
Michael Salgado <elesdoar@gmail.com>
2019-01-04 21:45:02 -0500

    chore(ng-leaflet): remove main modules for refactor

--- dist/ui-leaflet-ng2.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export * from './components/leaflet.component';
-export * from './directives/center.directive';
-export * from './pipes/sample.pipe';
-export * from './services/defaults.service';
-export declare class UiLeafletModule {
-    static forRoot(): ModuleWithProviders;
-}
--- docs/app/app.component.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AppComponent {
-    name: string;
-    center: {
-        lat: number;
-        lng: number;
-        zoom: number;
-    };
-    changeCenter(city: string): void;
-}
